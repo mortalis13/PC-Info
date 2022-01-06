75afa3f80eaefdf948bcbbaa50146bbca0f4e9f5
mortalis13 <stangerroman@gmail.com>
2022-01-06 14:53:29 +0100

    mp3 script edit

--- ffmpeg/ffmpeg-convert-audio.bat
+++ ffmpeg/ffmpeg-convert-audio.bat
@@ -1,6 +1,8 @@
 @echo off
 echo.
 
+rem fm -y -hide_banner -i "input.flac" -c:a libmp3lame -b:a 320k -ar 44100 -map_metadata 0 -id3v2_version 3 "output.mp3"
+
 set ORIG_DIR=_orig
 
 set f_aac="%cd%\*.aac"
@@ -14,6 +16,8 @@ set f_ogg="%cd%\*.ogg"
 set f_wav="%cd%\*.wav"
 set f_wma="%cd%\*.wma"
 set f_ac3="%cd%\*.ac3"
+set f_dsf="%cd%\*.dsf"
+set f_webm="%cd%\*.webm"
 
 set bitrate=320
 
@@ -26,15 +30,22 @@ if not "%1"=="" (
 )
 echo.
 
+if %bitrate%==0 (
+  echo -- using best quality (220-260 kbps)
+  set quality=-q 0
+) else (
+  set quality=-b:a %bitrate%k
+)
+
 mkdir %ORIG_DIR%
 
-for %%a in (%f_aac% %f_aiff% %f_ape% %f_flac% %f_m4a% %f_m4b% %f_m4p% %f_ogg% %f_wav% %f_wma% %f_ac3%) do (
+for %%a in (%f_aac% %f_aiff% %f_ape% %f_flac% %f_m4a% %f_m4b% %f_m4p% %f_ogg% %f_wav% %f_wma% %f_ac3% %f_dsf% %f_webm%) do (
   rem echo %%a
   rem echo %%~na
   rem echo %%~nxa
   
   move "%%~nxa" "%ORIG_DIR%\"
-  fm -y -hide_banner -i "%ORIG_DIR%\%%~nxa" -codec:a libmp3lame -b:a %bitrate%k -ar 44100 -map_metadata 0 -id3v2_version 3 "%%~na.mp3"
+  fm -hide_banner -y -i "%ORIG_DIR%\%%~nxa" -c:a libmp3lame %quality% -ar 44100 -map_metadata 0 -id3v2_version 3 "%%~na.mp3"
 )
 
 echo.
