29b03291c364802d5e7c6017ceec733fd9e14ec9
mortalis13 <stangerroman@gmail.com>
2026-01-07 16:11:50 +0100

    .

--- ffmpeg/ffmpeg-convert-audio.bat
+++ ffmpeg/ffmpeg-convert-audio.bat
@@ -1,10 +1,10 @@
 @echo off
 echo.
 
-rem fm -y -hide_banner -i "input.flac" -c:a libmp3lame -b:a 320k -ar 44100 -map_metadata 0 -id3v2_version 3 "output.mp3"
+rem fm -hide_banner -y -i "input.flac" -c:a libmp3lame -b:a 320k -ar 44100 -map_metadata 0 -id3v2_version 3 "output.mp3"
 
-set ORIG_DIR=_orig
-set TYPES=aac ape m4a m4b m4p ogg wav wma ac3 dsf dts aiff flac webm mp4 mkv
+set TMP_DIR=c:\ctmp
+set TYPES=aac ape m4a m4b m4p ogg wav wma ac3 dsf dts aiff flac webm mp4 mkv vob opus
 set BIT_RATE=320
 
 echo.
@@ -23,7 +23,7 @@ if %BIT_RATE%==0 (
   set quality=-b:a %BIT_RATE%k
 )
 
-mkdir %ORIG_DIR%
+mkdir %TMP_DIR%
 
 for %%x in (%TYPES%) do (
   for %%a in ("%cd%\*.%%x") do (
@@ -31,8 +31,8 @@ for %%x in (%TYPES%) do (
     rem echo %%~na
     rem echo %%~nxa
     
-    move "%%~nxa" "%ORIG_DIR%\"
-    fm -hide_banner -y -i "%ORIG_DIR%\%%~nxa" -c:a libmp3lame %quality% -ar 44100 -map_metadata 0 -id3v2_version 3 "%%~na.mp3"
+    move "%%~nxa" "%TMP_DIR%\"
+    fm -hide_banner -y -i "%TMP_DIR%\%%~nxa" -c:a libmp3lame %quality% -ar 44100 -map_metadata 0 -id3v2_version 3 "%%~na.mp3"
   )
 )
 echo.
